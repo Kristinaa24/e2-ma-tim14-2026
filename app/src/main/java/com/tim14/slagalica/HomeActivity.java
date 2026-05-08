20bcca353bd1cb448af3bee07f50e3d708864aba
Kristina Divnic <kristinadivnic2003@gmail.com>
2026-05-09 01:01:38 +0200

    Add navigation drawer and redesign home screen UI

--- app/src/main/java/com/tim14/slagalica/HomeActivity.java
+++ app/src/main/java/com/tim14/slagalica/HomeActivity.java
@@ -4,37 +4,72 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button avatarProfileButton;
     private Button startGameButton;
     private Button koZnaZnaButton;
     private Button spojniceButton;
     private Button korakPoKorakButton;
     private Button mojBrojButton;
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar homeToolbar;
+    private ActionBarDrawerToggle drawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        avatarProfileButton = findViewById(R.id.avatarProfileButton);
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+        homeToolbar = findViewById(R.id.homeToolbar);
+
+        setSupportActionBar(homeToolbar);
+
+        drawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                homeToolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_profile) {
+                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
+            }
+            else if (id == R.id.nav_login) {
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+            else if (id == R.id.nav_register) {
+                startActivity(new Intent(HomeActivity.this, RegisterActivity.class));
+            }
+
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
+
         startGameButton = findViewById(R.id.startGameButton);
         koZnaZnaButton = findViewById(R.id.koZnaZnaButton);
         spojniceButton = findViewById(R.id.spojniceButton);
         korakPoKorakButton = findViewById(R.id.korakPoKorakButton);
         mojBrojButton = findViewById(R.id.mojBrojButton);
-
-        if (SessionManager.currentUser == null) {
-            SessionManager.currentUser =
-                    new User("TestPlayer", "test@mail.com", "Vojvodina", 5, 120, 2);
-        }
-
-        avatarProfileButton.setOnClickListener(v ->
-                startActivity(new Intent(HomeActivity.this, ProfileActivity.class))
-        );
 
         startGameButton.setOnClickListener(v ->
                 startActivity(new Intent(HomeActivity.this, KoZnaZnaActivity.class))
