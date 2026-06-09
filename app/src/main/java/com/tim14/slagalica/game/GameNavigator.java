a6d8386ceae22c58e68862efc69c0b647e973a5b
Aleksandra Djuric <anjafpobe2003@gmail.com>
2026-06-09 14:47:51 +0200

    feat: add shared fragment-based match host

---
+++ app/src/main/java/com/tim14/slagalica/game/GameNavigator.java
@@ -0,0 +1,14 @@
+package com.tim14.slagalica.game;
+
+import android.os.Bundle;
+
+public interface GameNavigator {
+    void setPhaseText(String text);
+    void setTimerValue(int seconds);
+    void setScores(int playerOne, int playerTwo);
+    int getPlayerOneScore();
+    int getPlayerTwoScore();
+    void goToRound(GameRound round, Bundle args);
+    void goToNextRound();
+    void finishMatch();
+}
